89b54044af4f0af8c10b26f9d9caedbdf2d70b58
yayang <justforyy@163.com>
2023-11-16 10:23:51 +0800

    删除部分模型

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear
+from models import Informer, Autoformer, Transformer, DLinear
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
@@ -28,9 +28,7 @@ class Exp_Main(Exp_Basic):
             'Autoformer': Autoformer,
             'Transformer': Transformer,
             'Informer': Informer,
-            'DLinear': DLinear,
-            'NLinear': NLinear,
-            'Linear': Linear,
+            'DLinear': DLinear
         }
         model = model_dict[self.args.model].Model(self.args).float()
 
